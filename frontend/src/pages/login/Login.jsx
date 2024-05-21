4cb68d0a6fa8e2d9d448326fc4458825306c8e62
Andrew Cosmic <acdickin@gmail.com>
2024-05-21 14:49:17 -0700

    added tailwind and daisyui

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'>Login</h1>
+            </div>
+            <button className="btn">Button</button>
+            <button className="btn btn-neutral">Neutral</button>
+            <button className="btn btn-primary">Primary</button>
+            <button className="btn btn-secondary">Secondary</button>
+            <button className="btn btn-accent">Accent</button>
+            <button className="btn btn-ghost">Ghost</button>
+            <button className="btn btn-link">Link</button>
+        </div>
+    )
+}
+
+export default Login
