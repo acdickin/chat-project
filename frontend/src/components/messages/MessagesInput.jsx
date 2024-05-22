2c4e5f5a6262abb39083825b4a7830241006bcd0
Andrew Cosmic <acdickin@gmail.com>
2024-05-22 12:02:41 -0700

    frontend without backend set up

--- frontend/src/components/messages/MessagesInput.jsx
+++ frontend/src/components/messages/MessagesInput.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { BsSend } from "react-icons/bs";
 const MessagesInput = () => {
     return (
-        <div>MessagesInput</div>
+        <form className='px-4 my-3 '>
+            <div className='w-full flex'>
+                <input
+                    type="text"
+                    className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white'
+                    placeholder='Send a message'
+                />
+                <button type="submit" className='p-3'>
+                    <BsSend />
+                </button>
+            </div>
+        </form>
     )
 }
 
--- frontend/src/pages/home/MessageContainer.jsx
+++ frontend/src/pages/home/MessageContainer.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import Messages from '../../components/messages/Messages'
 import MessagesInput from '../../components/messages/MessagesInput'
+import { TiMessages } from "react-icons/ti";
 const MessageContainer = () => {
+    const noChatSelected = true;
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            <>
-                <div className='bg-sky-500 px-4 py-2 mb-2'>
-                    <span className='label-text'> To: </span>{""}
-                    <span className='text-b font-bold'>John Doe</span>
-                </div>
-                <Messages />
-                <MessagesInput />
-            </>
+            {noChatSelected
+                ? (<NoChatSelected />)
+                : (
+                    <>
+                        <div className='bg-sky-500 px-4 py-2 mb-2'>
+                            <span className='label-text'> To: </span>{""}
+                            <span className='text-b font-bold'>John Doe</span>
+                        </div>
+                        <Messages />
+                        <MessagesInput />
+                    </>
+                )
+            }
+        </div>
+    )
+}
+
+const NoChatSelected = () => {
+    return (
+        <div className='flex items-center justify-center w-full h-full'>
+            <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+                <p>Welcome John Doe</p>
+                <p>Select a chat to start messaging</p>
+                <TiMessages className='3xl md:text-6xl text-center' />
+            </div>
         </div>
     )
 }
