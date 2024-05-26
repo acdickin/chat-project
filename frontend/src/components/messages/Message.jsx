cd3b1506b70b0f7d5b3e75574028d6640980c814
Andrew Cosmic <acdickin@gmail.com>
2024-05-26 14:01:57 -0700

    Made backend static for the build. Small style changes

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -7,9 +7,9 @@ const Message = ({ messageData }) => {
     const { selectedConversation } = useConversation();
     const fromMe = messageData.senderId === authUser._id;
     const formattedTime = extractTime(updatedAt);
-    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const chatClassName = fromMe ? "chat-end " : "chat-start";
     const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-    const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+    const bubbleBgColor = fromMe ? "bg-green-400" : "";
     const shakeClass = message.shouldShake ? "shake" : "";
 
 
@@ -21,7 +21,7 @@ const Message = ({ messageData }) => {
                     <img alt="Tailwind CSS chat bubble component" src={profilePic} />
                 </div>
             </div>
-            <div className={`chat-bubble chat-bubble-info ${bubbleBgColor} ${shakeClass}`} >{message}</div>
+            <div className={`chat-bubble chat-bubble-info ${bubbleBgColor} ${shakeClass} text-white`} >{message}</div>
             <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
         </div>
     )
